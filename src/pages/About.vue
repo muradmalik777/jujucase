9d7dbdd17df44b1c388192b9677a8a4adb841f1e
aliakbarrehman <14bscsarehman@seecs.edu.pk>
2019-03-25 22:56:06 +0500

    Initial Commit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,33 @@
 <template>
     <div class="about spacing">
-        <h1>About</h1>
+        <div class="m-b-4">
+            <h1 class="m-b">What is this website?</h1>
+            <div class="bg-purple-dull para-padding">
+                <p>We offer a much better alternative to in-game cases or cates offered by games such as PlayerUnknown's Battlegrounds or Counter-Strike: Global Offensive. Our website makes the odds public, has a valid provably fair system and even offers a case creator feature which gives users the opportunity to create a cases with only the items they want an with what odds they prefer.</p>
+            </div>
+        </div>
+
+        <div class="m-b-4">
+            <h1 class="m-b">Case Opening</h1>
+            <div class="bg-purple-dull para-padding">
+                <p>In video games, a case (sometimes crate or loot box as well as other names) is a consumable virtual item which can be redeemed to receive a randomised selection of further virtual items, ranging from simple customization options for a player's avatar or character, to game-changing equipment such as weapons and armor. A loot box is typically a form of monetisation, with players either buying the boxes directly or receiving the boxes during play and later buying 'keys' with which to redeem them. These systems may also be known as gacha (based on gashapon - capsule toys and integrated into gacha games.
+                </p>
+                <br>
+                <p>
+                Loot box concepts originated from loot systems in massively multiplayer online role-playing games, and from the monetization of free-to-play mobile gaming. They first appeared around 2007 to 200, but since then, appear in many free-to-play games and in some full-priced title.</p>
+            </div>
+        </div>
+
+        <div class="m-b-4">
+            <h1 class="m-b">Counter-Strike: Global Offensive</h1>
+            <div class="bg-purple-dull para-padding">
+                <p>Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person shooter video game developed by Hidden Path Entertainment and Valve Corporation. It is the fourth game in the Counter-Strike series and was released for Microsoft Windows, OS X, Xbox 360 and Playstaion 3 in August 2012, With the Linux version released in September 2014. The game pits two teams against each other: the terrorists and the Counter-Terrorists. Both sides are tasked with eliminating the other while also completing seperate objectives, the Terrorists depending on the game mode, must either plant the bomb or defend the hostages, while the Counter-Terrorists must either prevent the bomb from being planted, defuse the bomb or rescue the hostages. There are six game modes, all of which have distinct characteristics specific to that mode.
+                </p>
+                <br>
+                <p>
+                Global Offensive has matchmaking support that allows players to play on dedicated Valve servers, as well as allowing members of the community to host their own server with custom maps and game modes. Global Offensive has a competitive scene, with Valve-sponsored tournaments being the premier competitive events for the game. Global Offensive received positive reviews from citics. The game was praised for its overall gameplay and faithfulness to the previous iterations in the series. Some of the early features were criticized, and while the console versions received positive reviews. Reviewers believed there were obvious differences between the console and PC versions of the game.</p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +35,8 @@ export default {
     name: 'about',
 }
 </script>
-<style lang="scss">
-
+<style lang="scss" scoped>
+    h1 {
+        font-weight: 600;
+    }
 </style>
--- src/pages/TOS.vue
+++ src/pages/TOS.vue
@@ -1,6 +1,127 @@
 <template>
     <div class="tos spacing">
-        <h1>TOS</h1>
+        <h1 class="m-b">Terms of Service</h1>
+        <div class="bg-purple-dull para-padding m-b-4">
+            <p>No individual under the age of eighteen (18) may use UnboxSkins, regardless of any consent from a parent or guardian to use UnboxSkins.</p>
+            
+            <p>You need a supported Web browser to access UnboxSkins. You acknowledge and agree that UnboxSkins may cease to support a given Web browser and that your continuous use of UnboxSkins will require you to download a supported Web browser. You also acknowledge and agree that the performance of UnboxSkins is incumbent on the performance of your computer equipment and your Internet connection.</p>
+            
+            <p>You agree to sign on and register for UnboxSkins through your Steam account provided by Valve Corporation. You are solely responsible for managing your account and  password and for keeping your password confidential. You are also solely responsible for restricting access to your account.</p>
+            
+            <p> You agree that you are responsible for all activities that occur on your account or  through the use of your password by yourself or by other persons. If you believe that  a third party has access your password, use the password regeneration feature of UnboxSkins as soon as possible to obtain a new password. In all circumstances, you agree not to permit any third party to use or access UnboxSkins.</p>
+            
+            <h4 class="bold">As a condition to your use of UnboxSkins, you agree not to:</h4>
+            <p class="with-circle">
+                <span class="circle">A</span>
+                <span class="circle-text">impersonate or misrepresent your affiliation with any person or entity:</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">B</span>
+                <span class="circle-text">access, tamper with, or use any non-public areas of UnboxSkins or UnboxSkins's computer systems;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">C</span>
+                <span class="circle-text">attempt to probe, scan, or test the vulnerability of UnboxSkins or any related system or network or breach any security or authentication measures used in connection with UnboxSkins and such systems and networks;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">D</span>
+                <span class="circle-text">attempt to decipher, decompile, disassemble, reverse engineer or otherwise investigate any of the software or components used to provide UnboxSkins;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">E</span>
+                <span class="circle-text">harm or threaten to harm other users in any way or interfere with, or attempt to interfere with, the access of any user, host or network, including without limitation, by sending a virus, overloading, flooding, spamming, or  mail-bombing UnboxSkins;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">F</span>
+                <span class="circle-text">provide payment information belonging to a third party:</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">G</span>
+                <span class="circle-text">use UnboxSkins in an abusive way contrary to its intended use, to UnboxSkins's policies and instructions and to any applicable law;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">H</span>
+                <span class="circle-text">systematically retrieve data or other content from  UnboxSkins to create or compile, directly or indirectly, in single or multiple downloads, a collection, compilation, database, directory or the like, whether by manual methods, through the use of bots, crawlers, or spiders, or otherwise:</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">I</span>
+                <span class="circle-text">make use of UnboxSkins in a manner contrary to the terms and conditions under which third parties provide facilities and technology necessary for the operation of UnboxSkins, such as PAYPAL, G2A, BITPAY or VALVE CORP;</span>
+            </p>
+            
+            <p class="with-circle">
+                <span class="circle">J</span>
+                <span class="circle-text">infringe third party intellectual property rights when using or accessing UnboxSkins, including but not limited to in making available virtual items by using UnboxSkins;</span>
+            </p>
+            
+            <p>
+                and (k) make use of, promote, link to or provide access to materials deemed by UnboxSkins at its sole discretion to be offensive or cause harm to UnboxSkins’s reputation, including, but not limited to, illegal content and pornographic content and content deemed offensive or injurious to UnboxSkins (such as Warez sites, IRC bots and bittorent sites).
+            </p>
+            
+            <h4 class="bold">Termination</h4>
+            <p>
+                We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms of Service.
+                <br>
+                <br>
+                All provisions of the Terms of Service which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+                <br>
+                <br>
+                We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms of Service.
+                <br>
+                <br>
+                Upon termination, your right to use UnboxSkins will immediately cease. If you wish to  terminate your account, you may simply do so by discontinuing your use of UnboxSkins.
+            </p>
+            
+            <h4 class="bold">Links To Other Web Sites</h4>
+            <p>
+                Our Service may contain links to third-party web sites or services that are not owned or controlled by UnboxSkins. UnboxSkins has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that UnboxSkins shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.
+            </p>
+            
+            <p>
+                We strongly advise you to read the terms and conditions and privacy and policies of any third-party web sites or services that you visit.
+            </p>
+            
+            <h4 class="bold">No Warranties</h4>
+            <p>
+                This website is provided 'as is' without any representations or warranties, express or implied www.UnboxSkins.com makes no representations or warranties in relation to this website or the information and materials provided on this website. Without prejudice to the generality of the foregoing paragraph. www.UnboxSkins.com does not warrant that this website will be constantly available at all, or the information on this website in complete, true, accurate or non-misleading. Nothing on this website constitutes or is meant to constitute advice of any king. If you require advice in relation to any legal, financial or medical matter you should consult an appropriate professional.
+            </p>
+            
+            <h4 class="bold">Affiliation</h4>
+            <p>
+                We are in NO WAY affiliated with or endorsed by the Valve corporation, Steam or any other trademarks of the Valve corporation.
+            </p>
+            
+            <h4 class="bold">UnboxSkins Wallet</h4>
+            <p>
+                By topping up an amount to your UnboxSkins wallet, you accept that the credits received (labeled by the $ sign) are not worth real money and not subject to any refunds.
+            </p>
+            
+            <h4 class="bold">Returns and Refunds Policy</h4>
+            <p>
+                We do not issue refunds for digital products once the order is confirmed and the product is sent. We recommend contacting us for assistance if you experience any issues receiving or downloading our products.
+            </p>
+            
+            <h4 class="bold">UI / Interface errors</h4>
+            <p>
+                Interfaces glitches/manipulation in no way entitles the user to a certain item. All outcomes are generated by our provably fair system and awarded as such. The user agrees that the outcome will be determined by the provably fair system outcoume and any user interface errors do not entitle the user to that item.
+            </p>
+            
+            <h4 class="bold">Bots and Steam API</h4>
+            <p>
+                The trade offer request sent by our bots are subject to Steam Guard authentication and the buyer must have Steam Guard activated for at least 15 days on their smartphone so the trade is completed. Note that this is a Steam restriction. The skins won't expire but the user need to wait for that period to end.
+            </p>
+            <br>
+            <h4>
+                <span class="bold">For customer service inquiries or disputes, you may contact us by email at support@unboxskins.com</span>
+            </h4>
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +129,31 @@ export default {
     name: 'tos',
 }
 </script>
-<style lang="scss">
-
+<style lang="scss" scoped>
+    h1 {
+        font-weight: 600;
+    }
+    h4 {
+        margin: 0px;
+    }
+     .with-circle {
+         margin: 2rem 0px;
+        .circle {
+            vertical-align: top;
+            display: inline-block;
+            font-weight: 600;
+            width: 35px;
+            height: 35px;
+            text-align: center;
+            padding: 4px 0px;
+            margin-right: 15px;
+            border: 1px solid rgba(255, 255, 255, 0.35);
+            border-radius: 100%;
+        }
+        .circle-text {
+            vertical-align: top;
+            display: inline-block;
+            width: calc(100% - 50px);
+        }
+     }
 </style>
