45662690de083bd83a6d02115c0a3b26f6eb909b
aliakbarrehman <14bscsarehman@seecs.edu.pk>
2019-04-01 00:41:48 +0500

    Profile

--- src/router.js
+++ src/router.js
@@ -4,6 +4,10 @@ import Home from '@/pages/Home'
 import FAQ from '@/pages/FAQ'
 import TOS from '@/pages/TOS'
 import About from '@/pages/About'
+import Profile from '@/pages/Profile'
+import ProfileCases from '@/components/Profile/Cases'
+import ProfileTrades from '@/components/Profile/Trades'
+import ProfileTransactions from '@/components/Profile/Transactions'
 
 
 Vue.use(Router);
@@ -15,6 +19,13 @@ export default new Router({
         { path: '/', component: Home },
         { path: '/about', component: About },
         { path: '/faq', component: FAQ },
-        { path: '/tos', component: TOS }
+        { path: '/tos', component: TOS },
+        { path: '/profile', component: Profile,
+            children: [
+                { path: 'cases', component: ProfileCases },
+                { path: 'trades', component: ProfileTrades },
+                { path: 'cases', component: ProfileTransactions }
+            ]
+        }
     ]
 })
