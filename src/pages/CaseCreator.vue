567db58fa83831105d68099b266892fd1784ee01
Murad Malik <muradmalik7@gmail.com>
2019-05-05 13:33:59 +0500

    case creator design fixes

--- src/pages/CaseCreator.vue
+++ src/pages/CaseCreator.vue
@@ -6,7 +6,7 @@
                 <v-text-field class="case-name-input" placeholder="Enter case name" type="text" full-width v-model="new_case.name"></v-text-field>
             </v-flex>
         </v-layout>
-        <v-layout row wrap pt-3>
+        <v-layout row wrap>
             <v-flex xs12>
                 <h3 class="uppercase">case picture</h3>
             </v-flex>
@@ -19,7 +19,7 @@
                 <h3 class="uppercase">add skins</h3>
             </v-flex>
             <v-flex xs10 class="search">
-                <v-autocomplete class="skin-search m-t-2" placeholder="search" v-model="search" :items="search_result"></v-autocomplete>
+                <v-autocomplete class="skin-search m-t" placeholder="search" v-model="search" :items="search_result"></v-autocomplete>
             </v-flex>
             <v-flex xs1 class="select">
                 <v-select background-color="#73337a" color="#fff" :value="items[0]" :items="items"></v-select>
@@ -212,17 +212,17 @@ export default {
     }
     .case-image-box{
         margin: 1rem 1rem;
-        min-height: 200px;
+        min-height: 150px;
         .case-picture{
             width: 150px;
             height: 100px;
             cursor: pointer;
             display: block;
-            margin: 3.5rem auto;
+            margin: 1.5rem auto;
         }
     }
     .select{
-        margin-top: 1.7rem;
+        margin-top: .7rem;
     }
     .skin{
         min-height: 370px;
