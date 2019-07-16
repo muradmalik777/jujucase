520a681c230d2452daff61a44e013dfa80c66de1
Murad Malik <muradmalik7@gmail.com>
2019-07-16 11:01:32 +0500

    user verification

--- src/components/UserVerification.vue
+++ src/components/UserVerification.vue
@@ -1,21 +1,27 @@
 <template>
     <v-container grid-list-xl class="verify-user spacing">
-        <v-layout row wrap justify-center>
-            <v-flex xs12 md8 lg5 class="verify-form">
-                <h2 class="uppercase m-b-2">verify your result</h2>
-                <v-form ref="verifyForm">
-                    <label>Name</label>
-                    <v-text-field color="#fff" v-model="verify.name" background-color="#67266e" class="m-b" :rules="dataRules" required></v-text-field>
-                    <label>Surname</label>
-                    <v-text-field color="#fff" v-model="verify.surname" background-color="#67266e" class="m-b" :rules="dataRules" required></v-text-field>
-                    <label>ID Card/Passport</label>
-                    <input color="#fff" type="file" class="m-b-2 m-t" :rules="dataRules" required @change="setImage($event)"/>
-                    <label>Date of Birth</label>
-                    <v-date-picker color="#67266e" v-model="verify.dob" class="m-b date" :landscape="false" :reactive="false"></v-date-picker>
-                    <v-btn :loading="loading" @click="verifyUser" class="verify-btn" flat>Verify</v-btn>
-                </v-form>
-            </v-flex>
-        </v-layout>
+      <v-layout row wrap justify-center v-if="$store.state.userData.has_applied_verification">
+        <v-flex xs12 md10 lg10 class="verify-form">
+          <h1>User Account Verification</h1>
+          <h3 class="m-b-2">We will verify your age based upon the document you uploaded, after that you can withdraw your items.</h3>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap justify-center >
+          <v-flex xs12 md8 lg5 class="verify-form">
+              <h2 class="uppercase m-b-2">verify your result</h2>
+              <v-form ref="verifyForm">
+                  <label>Name</label>
+                  <v-text-field color="#fff" v-model="verify.name" background-color="#67266e" class="m-b" :rules="dataRules" required></v-text-field>
+                  <label>Surname</label>
+                  <v-text-field color="#fff" v-model="verify.surname" background-color="#67266e" class="m-b" :rules="dataRules" required></v-text-field>
+                  <label>ID Card/Passport</label>
+                  <input color="#fff" type="file" class="m-b-2 m-t" :rules="dataRules" required @change="setImage($event)"/>
+                  <label>Date of Birth</label>
+                  <v-date-picker color="#67266e" v-model="verify.dob" class="m-b date" :landscape="false" :reactive="false"></v-date-picker>
+                  <v-btn :loading="loading" @click="verifyUser" class="verify-btn" flat>Verify</v-btn>
+              </v-form>
+          </v-flex>
+      </v-layout>
     </v-container>
 </template>
 <script>
@@ -26,7 +32,11 @@ export default {
   data: function() {
     return {
       verify: {
-        dob: new Date().toISOString().substr(0, 10)
+        dob: new Date().toISOString().substr(0, 10),
+        supported_types : ["id_card","driving_license","passport"],
+        issue_date: "", 
+        expiry_date: "",
+        document_number: ""
       },
       loading: false,
       dataRules: [v => !!v || "The input is required"]
@@ -34,7 +44,6 @@ export default {
   },
   methods: {
     verifyUser: function(){
-      console.log(this.verify)
       this.loading= true
       let $object = new Api('/user/verify')
       let params = {
@@ -43,15 +52,21 @@ export default {
       }
       $object.post(params).then(response =>{
         this.loading = false
-        console.log(response)
+        this.$store.commit('addUser', response.user)
       }).catch((error)=>{
         this.loading = false
         this.showMessage(error.response.data.message)
       })
     },
     setImage: function(event){
-      this.verify.image = event.target.files[0]
-    }
+      var self = this;
+      var file = event.target.files[0];
+        var reader  = new FileReader();
+        reader.onloadend = function () {
+            self.verify.proof = reader.result;
+        }
+      reader.readAsDataURL(file);
+    },
   }
 };
 </script>
